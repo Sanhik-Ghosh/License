85bd908056a32fa6e15b0ecd2404f7b9d7901944
Sanhik Ghosh <sanhikghosh2021@gmail.com>
2026-04-29 12:58:35 +0530

    Fix eligibility check to return True for eligible users

--- License.py
+++ License.py
@@ -14,7 +14,7 @@ class LicenseSystem:
 
     def check_eligibility(self, user):
         if user.age >= 18 and user.test_passed:
-            return False
+            return True
         return False
 
     def generate_license_number(self):
